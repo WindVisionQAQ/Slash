80021dd0396094eaa26bf0846205731595bcf732
Hephaestus <luisliuzijie@foxmail.com>
2025-11-29 12:10:43 +0800

    Add breakable actor base class.

---
+++ Source/Slash/Private/Breakable/BreakableActor.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Breakable/BreakableActor.h"
+#include "GeometryCollection/GeometryCollectionComponent.h"
+
+ABreakableActor::ABreakableActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	GeometryCollection = CreateDefaultSubobject<UGeometryCollectionComponent>(TEXT("GeometryCollection"));
+	SetRootComponent(GeometryCollection);
+	if (GeometryCollection)
+	{
+		GeometryCollection->SetGenerateOverlapEvents(true);
+		GeometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	}
+}
+
+void ABreakableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABreakableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABreakableActor::GetHit(const FVector& ImpactPoint)
+{
+
+}
+
+
